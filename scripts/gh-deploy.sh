3a04722da09a75503143d2c25d0354a411574daf
Kevin Gutierrez <kegutierrez@google.com>
2020-02-17 13:59:59 -0500

    Add gh-pages homepage

--- scripts/gh-deploy.sh
+++ scripts/gh-deploy.sh
@@ -1,13 +1,7 @@
-STATUS="$(git status)"
-
-if [[ $STATUS == *"nothing to commit"* ]]
-then
-    sed -i "" '/build/d' ./.gitignore
-    git add .
-    git commit -m "Edit .gitignore to publish"
-    git push origin `git subtree split --prefix build master`:gh-pages --force
-    git reset HEAD~
-    git checkout .gitignore
-else
-    echo "Need clean working directory to publish"
-fi
+#!/bin/sh
+sed -i "" '/build/d' ./.gitignore
+git add .
+git commit -m "Edit .gitignore to publish"
+git push origin `git subtree split --prefix build master`:gh-pages --force
+git reset HEAD~
+git checkout .gitignore
