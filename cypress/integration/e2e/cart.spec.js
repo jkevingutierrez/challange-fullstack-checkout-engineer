939932db0b63480cd244adb67662a1642883b2bd
Kevin Gutierrez <kgutierrez@hugeinc.com>
2020-02-17 08:11:19 -0500

    Add cypress

---
+++ cypress/integration/e2e/cart.spec.js
@@ -0,0 +1,5 @@
+describe('Cart Route', () => {
+  it('visits the cart page', () => {
+    cy.visit('/cart')
+  });
+});
---
+++ cypress/integration/e2e/home.spec.js
@@ -0,0 +1,5 @@
+describe('Home Route', () => {
+  it('visits the home page', () => {
+    cy.visit('/')
+  });
+});
