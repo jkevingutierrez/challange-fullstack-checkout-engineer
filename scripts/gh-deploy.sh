9ff98f96156a0b5a9d6015e9797003277674467a
Kevin Gutierrez <kegutierrez@google.com>
2020-02-17 12:24:10 -0500

    Fix gh-pages deploy

--- scripts/gh-deploy.sh
+++ scripts/gh-deploy.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
-if [ -z "$1" ]
-then
-  echo "Which folder do you want to deploy to GitHub Pages?"
-  exit 1
-fi
+STATUS="$(git status)"
 
-git subtree push --prefix $1 origin gh-pages
+if [[ $STATUS == *"nothing to commit"* ]]
+then
+    sed -i "" '/build/d' ./.gitignore
+    git add .
+    git commit -m "Edit .gitignore to publish"
+    git push origin `git subtree split --prefix build master`:gh-pages --force
+    git reset HEAD~
+    git checkout .gitignore
+else
+    echo "Need clean working directory to publish"
+fi
