3d60779eee1118609abbd40e3d3e8974e678a49d
Kevin Gutierrez <kegutierrez@google.com>
2020-02-17 14:16:54 -0500

    use gh-pages

--- scripts/gh-deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sed -i "" '/build/d' ./.gitignore
-git add .
-git commit -m "Edit .gitignore to publish"
-git push origin `git subtree split --prefix build master`:gh-pages --force
-git reset HEAD~
-git checkout .gitignore
