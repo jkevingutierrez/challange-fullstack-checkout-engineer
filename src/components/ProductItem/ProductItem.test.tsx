8c0c61278a3b0681b80fc971ca55c28683780f57
Kevin Gutierrez <kgutierrez@hugeinc.com>
2020-02-18 00:26:15 -0500

    Fix tests mocking IntersectionObserver

--- src/components/ProductItem/ProductItem.test.tsx
+++ src/components/ProductItem/ProductItem.test.tsx
@@ -4,6 +4,25 @@ import ProductItem from './ProductItem';
 
 afterEach(cleanup);
 
+const observeMock = {
+  observe: () => null,
+  disconnect: () => null // maybe not needed
+};
+
+beforeEach(() => {
+  const global = window as any;
+
+  global.IntersectionObserver = class IntersectionObserver {
+    observe() {
+      return null;
+    }
+
+    unobserve() {
+      return null;
+    }
+  };
+});
+
 test('ProductItem empty', () => {
   const { getByText } = render(<ProductItem product={{ productId: '' }}/>);
 });
