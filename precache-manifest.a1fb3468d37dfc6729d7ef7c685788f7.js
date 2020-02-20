6d1302adb59c313a3440bb61ba8ff87bdc95509b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-20 15:18:20 +0000

    Deploy jkevingutierrez/challange-fullstack-checkout-engineer to github.com/jkevingutierrez/challange-fullstack-checkout-engineer.git:gh-pages

---
+++ precache-manifest.a1fb3468d37dfc6729d7ef7c685788f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f9a1fe84a82466ee90dcd4a6103af0f",
+    "url": "/challange-fullstack-checkout-engineer/index.html"
+  },
+  {
+    "revision": "7c9b6477376f06a0e622",
+    "url": "/challange-fullstack-checkout-engineer/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "3a56a0d1644b8a5106af",
+    "url": "/challange-fullstack-checkout-engineer/static/css/main.5bdd4331.chunk.css"
+  },
+  {
+    "revision": "7c9b6477376f06a0e622",
+    "url": "/challange-fullstack-checkout-engineer/static/js/2.e6280077.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/challange-fullstack-checkout-engineer/static/js/2.e6280077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a56a0d1644b8a5106af",
+    "url": "/challange-fullstack-checkout-engineer/static/js/main.5401922a.chunk.js"
+  },
+  {
+    "revision": "40f56193599c47e93ed3",
+    "url": "/challange-fullstack-checkout-engineer/static/js/runtime-main.58e41044.js"
+  }
+]);
